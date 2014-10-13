0315f92ab99b789f5d070144a9ec8ba36a5a5e0c
Roy McFarland <roy.mcfarland@gmail.com>
2014-10-13 14:01:52 -0600

    first commit

---
+++ islight.js
@@ -0,0 +1,16 @@
+var r = process.argv[2];
+var g = process.argv[3];
+var b = process.argv[4];
+var luminosity = 0.2126*r + 0.7152*g + 0.0722*b;
+
+console.log(luminosity);
+
+var islight = function(luminosity){
+	if (luminosity >= 155) {
+		return 'light';
+	} else {
+		return 'dark';
+	};
+};
+
+console.log(islight(luminosity));
---
+++ luminosity-cli.js
@@ -0,0 +1,7 @@
+var r = process.argv[2];
+var g = process.argv[3];
+var b = process.argv[4];
+
+var colorutil = require('./colorutil.js');
+
+console.log(colorutil.luminosity(r, g, b));
